7f3d826590e8c87cd936902f866ad8bf5b1a80d6
Liam Donnelly <donnelly1996@gmail.com>
2017-09-29 12:43:09 +0100

    Completed FizzBuzzKata

--- src/FizzBuzz/FizzBuzz.cs
+++ src/FizzBuzz/FizzBuzz.cs
@@ -6,6 +6,25 @@ namespace FizzBuzz
     {
         public string Check(int i)
         {
+            string word = "";
+
+            if((i % 3) == 0 )
+            {
+                word += "Fizz";
+            }
+
+            if(((i % 5) == 0))
+            {
+                word += "Buzz";
+            }
+
+            if(word.Equals(""))
+            {
+                word +=  i.ToString();
+            }
+
+            return word;
+
             throw new NotImplementedException();
         }
     }
