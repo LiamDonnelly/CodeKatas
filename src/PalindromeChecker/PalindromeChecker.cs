5f591729b48b8a5f1b567e23a0082e912edf317e
Liam Donnelly <donnelly1996@gmail.com>
2017-09-30 21:05:14 +0100

    Completed PalindromeChecker kata

--- src/PalindromeChecker/PalindromeChecker.cs
+++ src/PalindromeChecker/PalindromeChecker.cs
@@ -6,6 +6,20 @@ namespace PalindromeChecker
     {
         public bool IsPalindrome(string s)
         {
+            if (!String.IsNullOrEmpty(s))
+            {
+                char[] arrOfString = s.ToCharArray();
+
+                Array.Reverse(arrOfString);
+                
+                if (!s.Equals(new string(arrOfString)))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+
             throw new NotImplementedException();
         }
     }
