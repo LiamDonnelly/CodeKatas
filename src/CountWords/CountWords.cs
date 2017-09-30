cd50c69de3c237d10170854d830f9e349e4ef1f3
Liam Donnelly <donnelly1996@gmail.com>
2017-09-30 20:54:53 +0100

    Completed CoundWordsTest kata

--- src/CountWords/CountWords.cs
+++ src/CountWords/CountWords.cs
@@ -6,6 +6,16 @@ namespace CountWords
     {
         public int Count(string s)
         {
+            if(!String.IsNullOrEmpty(s))
+            {
+                string[] arrayOfString = s.Split(',');
+
+                int count = arrayOfString.Length;
+                return count;
+            }
+
+            return 0;
+            
             throw new NotImplementedException();
         }
     }
