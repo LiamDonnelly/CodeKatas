7934a1c0fc3cccc995dd9db1c0fbbd77b01b36e7
LiamDonnelly <d018594e@student.staffs.ac.uk>
2017-10-01 19:51:22 +0100

    Completed PiCalculator

--- src/PiCalculator/PiCalculator.cs
+++ src/PiCalculator/PiCalculator.cs
@@ -6,11 +6,27 @@ namespace PiCalculator
     {
         public string GetPi(int i)
         {
+            int index = 2 + i;
+            char[] pi = Math.PI.ToString().ToCharArray();
+
+            Array.Resize(ref pi, index);
+
+            return new string(pi);
+
             throw new NotImplementedException();
         }
 
         public int GetPiDigit(int i)
         {
+            int piDigit = 0;
+            int index = 1 + i;
+
+            char[] pi = Math.PI.ToString().ToCharArray();
+
+            bool a = Int32.TryParse(pi[index].ToString(), out piDigit);
+
+            return piDigit;
+
             throw new NotImplementedException();
         }
     }
